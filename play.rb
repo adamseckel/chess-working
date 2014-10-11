f1339ea73000f64d72e7978581c8c599c289c3cf
aseckel <adam.seckel@gmail.com>
2014-10-10 23:27:41 -0700

    added input checks and quit function

--- play.rb
+++ play.rb
@@ -4,17 +4,56 @@ game = Game.new
 game.update_initial
 
 while game.piece_names2.include?("KK") && game.piece_names1.include?("KK")
-	print "Specify a position to move from (e.g. 9): "
-	x = gets.to_i
-	print "Specify a position to move to (e.g. 17): "
-	y = gets.to_i
+	print "Specify a position to move from (1-64 or 'quit'): "
+	x = gets.chomp
+	if  x.to_i > 0 && x.to_i < 65 
+		x = x
+	elsif x == "quit"
+		puts "    ~~ Game Over. Player 2 Wins. ~~"
+		break
+	else
+		print "Please type a valid move: "
+		x = gets.chomp
+	end
+
+	print "Specify a position to move to (1-64 or 'quit'): "
+	y = gets.chomp
+	if  y.to_i > 0 && y.to_i < 65 
+		y = y
+	elsif y == "quit"
+		puts "    ~~ Game Over. Player 2 Wins. ~~"
+		break
+	else
+		print "Please type a valid move: "
+		y = gets.chomp
+	end
 
 	game.move_player1(x,y)	
 
-	print "Specify a position to move from (e.g. 9): "
-	x = gets.to_i
-	print "Specify a position to move to (e.g. 17): "
-	y = gets.to_i
+	print "Specify a position to move from (1-64 or 'quit'): "
+	x = gets.chomp
+	if  x.to_i > 0 && x.to_i < 65 
+		x = x
+	elsif x == "quit"
+		puts "    ~~ Game Over. Player 1 Wins. ~~"
+		break
+	else
+		print "Please type a valid move: "
+		x = gets.chomp
+	end
+
+	print "Specify a position to move to (1-64 or 'quit'): "
+	y = gets.chomp
+	if  y.to_i > 0 && y.to_i < 65 
+		y = y
+	elsif y == "quit"
+		puts "    ~~ Game Over. Player 1 Wins. ~~"
+		break
+		
+	else
+		print "Please type a valid move: "
+		y = gets.chomp
+	end
 
 	game.move_player2(x,y)
 
