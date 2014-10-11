317bfe5f068f717471414678ef2cf07641afbf15
aseckel <adam.seckel@gmail.com>
2014-10-10 23:35:26 -0700

    oh sh*t I broke it...

--- play.rb
+++ play.rb
@@ -4,24 +4,28 @@ game = Game.new
 game.update_initial
 
 while game.piece_names2.include?("KK") && game.piece_names1.include?("KK")
-	print "Specify a position to move from (1-64 or 'quit'): "
+	print "Specify a position to move from (1-64/quit): "
 	x = gets.chomp
 	if  x.to_i > 0 && x.to_i < 65 
 		x = x
 	elsif x == "quit"
-		puts "    ~~ Game Over. Player 2 Wins. ~~"
+				puts
+		puts "       ~~ Game Over. Player 2 Wins. ~~"
+				puts
 		break
 	else
 		print "Please type a valid move: "
 		x = gets.chomp
 	end
 
-	print "Specify a position to move to (1-64 or 'quit'): "
+	print "Specify a position to move to (1-64/quit): "
 	y = gets.chomp
 	if  y.to_i > 0 && y.to_i < 65 
 		y = y
 	elsif y == "quit"
-		puts "    ~~ Game Over. Player 2 Wins. ~~"
+				puts
+		puts "       ~~ Game Over. Player 2 Wins. ~~"
+				puts
 		break
 	else
 		print "Please type a valid move: "
@@ -30,24 +34,28 @@ while game.piece_names2.include?("KK") && game.piece_names1.include?("KK")
 
 	game.move_player1(x,y)	
 
-	print "Specify a position to move from (1-64 or 'quit'): "
+	print "Specify a position to move from (1-64/quit): "
 	x = gets.chomp
 	if  x.to_i > 0 && x.to_i < 65 
 		x = x
 	elsif x == "quit"
-		puts "    ~~ Game Over. Player 1 Wins. ~~"
+				puts
+		puts "       ~~ Game Over. Player 1 Wins. ~~"
+				puts
 		break
 	else
 		print "Please type a valid move: "
 		x = gets.chomp
 	end
 
-	print "Specify a position to move to (1-64 or 'quit'): "
+	print "Specify a position to move to (1-64/quit): "
 	y = gets.chomp
 	if  y.to_i > 0 && y.to_i < 65 
 		y = y
 	elsif y == "quit"
-		puts "    ~~ Game Over. Player 1 Wins. ~~"
+		puts
+		puts "       ~~ Game Over. Player 1 Wins. ~~"
+		puts
 		break
 		
 	else
