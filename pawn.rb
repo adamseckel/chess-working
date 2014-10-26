933f71c1c8174bb2876640fad0afeb2a9ec19863
aseckel <adam.seckel@gmail.com>
2014-10-25 23:09:00 -0700

    Pawn logic

--- pawn.rb
+++ pawn.rb
@@ -3,18 +3,21 @@ require "./chesspieces.rb"
 class Pawn < ChessPieces
 
 	def move_set (x)
-		# if (x == @position + 8 && @player == 1) || (x == @position - 8 && @player == 2) 
-		# 	true
-		# elsif (x == @position + 16 && @player == 1) || (x == @position - 16 && @player == 2) 
-		# 	if @moves == 0
-		# 		true
-		# 	end
-		# elsif x == 0 
-		# 	true
-		# else
-		# 	false
-		# end
-		true
+		if (x == @position + 8 && @player == 1) || (x == @position - 8 && @player == 2)
+			true
+		elsif (x == @position + 7 && @player == 1) || (x == @position - 7 && @player == 2)
+			true		
+		elsif (x == @position + 9 && @player == 1) || (x == @position - 9 && @player == 2)
+			true
+		elsif (x == @position + 16 && @player == 1) || (x == @position - 16 && @player == 2) 
+			if @moves == 0
+				true
+			end
+		elsif x == 0 
+			true
+		else
+			false
+		end
 
 	end
 
